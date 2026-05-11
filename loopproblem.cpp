aef20c87fa0d53cbde1b37582e5bbf55e68ac54b
fahi112 <fa843294@gmail.com>
2026-05-11 20:51:55 +0600

    Create loopproblem.cpp

---
+++ loopproblem.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+int main(){
+int n=4;// # diamond
+ for(int i=1; i<=n; i++){
+     for(int j=1;j<=n-i; j++){
+         cout<<' ';
+     }
+    for(int j=1; j<=1; j++){
+         cout<< '*';
+     }
+     for(int j=1; j<2*(i-1); j++){
+         cout << ' ';
+     }
+     for(int j=1; j<=1; j++){
+         if(i!=1)
+         cout<<'*';
+     }
+     cout<< endl;
+ }
+ for(int i=1; i<=3; i++){
+     for(int j=1;j<=i; j++ ){
+         cout<<' ';
+     }
+     for(int j=1; j<=1; j++){
+         cout<< '*';
+     }
+     for(int j=0;j<=n-2*i;j++){
+         cout<< ' ';
+     }
+    for(int j=1; j<=1; j++){
+         if(i!=3)
+         cout<<'*';
+     }
+     cout<<endl;
+ }
+}
